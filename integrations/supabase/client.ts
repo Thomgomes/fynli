69e20e15e6d34dcef7e017ea04007f06161ad5dc
ThomGomes <math.011@hotmail.com>
2025-09-14 15:25:17 -0300

    remove: localstorage

--- integrations/supabase/client.ts
+++ integrations/supabase/client.ts
@@ -6,10 +6,4 @@ import { Database } from './types';
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const SUPABASE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
-  auth: {
-    storage: localStorage,
-    persistSession: true,
-    autoRefreshToken: true,
-  }
-});
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
