d1d1d91fa9bd466ca6e329886061c22443b2e084
ThomGomes <math.011@hotmail.com>
2025-09-14 17:31:35 -0300

    fix: google select_account login

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -75,7 +75,10 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback` 
+          redirectTo: `${window.location.origin}/auth/callback`,
+          queryParams: {
+            prompt: 'select_account',
+          }
         }
       });
       if (error) console.error("Erro no login com Google:", error);
